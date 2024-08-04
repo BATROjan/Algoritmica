dd22599d9836fd8dc9b776826c2c253fbfc5d2c7
BATROjan <batir0405200@gmail.com>
2024-08-04 15:56:15 +0700

    add project

---
+++ Assets/ConsoleWriteMessanger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ConsoleWriteMessanger : MonoBehaviour
+    {
+        private void Update()
+        {
+            Debug.Log("crickets");
+        }
+    }
+}
---
+++ Assets/CreatePrimitives.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CreatePrimitives : MonoBehaviour
+    {
+        [SerializeField] private Vector3[] points;
+        [SerializeField] private Transform steeveTransform;
+        
+        private void Start()
+        {
+            foreach (var point in points)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = point;
+                cube.transform.SetParent(steeveTransform, false);
+            }
+        }
+    }
+}
